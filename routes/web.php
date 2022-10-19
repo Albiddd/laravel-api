13d5d1470e5a7bf6cecfbfd06537aa8604b832e0
Albiddd <alberto.dore93@gmail.com>
2022-10-19 15:34:44 +0200

    add admin home

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware('auth')->get('/admin/home', 'Admin\HomeController@index')->name('admin.home');
